f3d75feb896bf1dd0bb68f62d3d9f6f23948f222
xtrmdarc <diegoreyesco@hotmail.com>
2020-03-04 15:55:09 -0500

    Add tests for prolog_at_start? method

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,11 +1,20 @@
 require './lib/xml_parser'
+require './lib/file_reader'
 
 describe XmlParser do
-  
-  let(:parser) { XmlParser.new }
+  let(:test_path) { './test_files/' }
+
   describe '#prolog_at_start?' do
-    it 'returns false if no well formed Prolog is found' do
-      true
+    it 'returns false if no well formed Prolog is found at the start of the file' do
+      xml_parser = XmlParser.new FileReader.get_content_from_file(test_path+'no_prolog.xml')
+      expect(xml_parser.prolog_at_start?).not_to eql(true)
     end
+
+    it 'returns true if well formed Prolog is found at the start of the file' do
+      xml_parser = XmlParser.new FileReader.get_content_from_file(test_path+'prolog.xml')
+      expect(xml_parser.prolog_at_start?).to eql(true)
+    end
+
   end
+
 end
