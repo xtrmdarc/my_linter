0151319b24fd309862dc8402e78a30844e380fae
xtrmdarc <diegoreyesco@hotmail.com>
2020-03-03 22:35:35 -0500

    Add methods to get node name and parse first prolog

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,14 @@
 #!/usr/bin/env ruby
+require './lib/xml_parser'
 
 file = File.open("./test.xml")
-content = file.read
+content = file.readlines.map(&:chomp)
+file.close
 
-p content
+xml_parser = XmlParser.new content
+result = xml_parser.prolog_at_start?
+result = xml_parser.well_formed_attributes?
+result = xml_parser.single_node?
+result = xml_parser.has_attributes?
+result = xml_parser.get_node_name
+p result
--- lib/xml_parser.rb
+++ lib/xml_parser.rb
@@ -1,3 +1,58 @@
 class XmlParser
   
+  def initialize(input)
+    @input = input
+    @multi_line_buffer = []
+  end
+  
+  def prolog_at_start?
+    puts @input[0]
+    if /^<\?xml(\s*[a-z]*=\".*\"\s*)*\?>$/ === @input[0]
+      puts "[TEST PASSED] : Prolog found"
+      return true
+    end
+    puts "Warning: No well formed prolog at the start of the file"
+    return false
+  end
+
+  def well_formed_attributes?(line= nil)
+    puts @input[1]
+    if /([a-z]+=\".+\")+/ === @input[1]
+      puts "[TEST PASSED] : All attributes are well formed"
+      return true
+    end
+    puts "[ERROR] : No well formed Attribute"
+    return false
+  end
+
+  def single_node?(line=nil)
+    puts @input[2]
+    if /^<[a-z]+\s*>/ === @input[2]
+      puts "[SINGLE NODE]"
+      return true
+    end
+    puts "NOT A SINGLE NODE"
+    false
+  end
+
+  def has_attributes?(line=nil)
+    puts @input[2]
+    if /^<[a-z]+\s+.+>/ === @input[2]
+      puts "[IT HAS ATTRUBTES]"
+      return true
+    end
+    puts "NO ATTRIBUTES"
+    false
+  end
+
+  def get_node_name
+    puts @input[2]
+    return @input[2][/^<\s*([a-z]+).*/, 1]
+  end
+
+  def parse
+    @input.each_with_index do |el, i|
+      
+    end
+  end
 end
